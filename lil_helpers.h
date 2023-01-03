f35c1909fe07ed2f6f787e28bb73709bb4964b4b
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2023-01-03 16:30:58 -0500

    verbose, add subcommand helpers
    subcommand helpers are unused as of yet

--- lil_helpers.h
+++ lil_helpers.h
@@ -1,14 +1,18 @@
 #ifndef LIL_HELPERS_H
 #define LIL_HELPERS_H
 
-#define LIL_FAILED(l, p...) do { char* b; asprintf(&b, p); lil_set_error(l, b); } while (0)
+#define LIL_FAILED(lil, printf_args...) do { char* temp_buf_; asprintf(&temp_buf_, printf_args); lil_set_error(lil, temp_buf_); } while (0)
 
 #define streq(s1, s2) (strcmp(s1, s2) == 0)
 
-#define LIL_FIXARITY(l, p, a, n) do { if (a != n) { LIL_FAILED(l, "Expected %i args to " p ", got %i", n, a); return NULL; } } while (0)
+#define LIL_FIXARITY(lil, cmd, argc, numargs) do { if (argc != numargs) { LIL_FAILED(lil, "Expected %i args to " cmd ", got %i", numargs, argc); return NULL; } } while (0)
 
-#define LIL_CHECKARGS(l, p, a, lo, hi) do { if (a < lo || a > hi) { LIL_FAILED(l, "Expected %i-%i args to " p ", got %i", lo, hi, a); return NULL; } } while (0)
+#define LIL_CHECKARGS(lil, cmd, argc, lo, hi) do { if (argc < lo || argc > hi) { LIL_FAILED(lil, "Expected %i-%i args to " cmd ", got %i", lo, hi, a); return NULL; } } while (0)
 
-#define LIL_NOARGS(l, p, a) do { if (a != 0) { LIL_FAILED(l, p " takes no arguments (got %i)", a); return NULL; } } while (0)
+#define LIL_NOARGS(lil, cmd, argc) do { if (argc != 0) { LIL_FAILED(l, cmd " takes no arguments (got %i)", argc); return NULL; } } while (0)
+
+#define LIL_HASSUBCOMMANDS(lil, topcmd, argc) do { if (argc == 0) { LIL_FAILED(lil, topcmd " needs a subcommand (got nothing)"); return NULL; } } while (0)
+
+#define LIL_SUBCOMMAND(lil, topcmd, subcmd, argc, argv, subcmd_func) if (streq(lil_to_string(argv[0]), subcmd)) return subcmd_func(lil, argc - 1, &argv[1]);
 
 #endif
